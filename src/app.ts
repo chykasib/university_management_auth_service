12c00b7116877ea70da59899e5ea0fd0dff0b624
Kasib Mohammad Chowdhury <kasib.md.chy@gmail.com>
2023-05-29 18:26:35 +0600

    setup express, mongoose, cors, parser

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Application, Request, Response, urlencoded } from "express";
+import cors from "cors";
+const app: Application = express();
+
+app.use(cors());
+// parser
+
+app.use(express.json());
+app.use(urlencoded({ extended: true }));
+// testing route
+app.get("/", (req: Request, res: Response) => {
+  res.send("working successfully");
+});
+
+export default app;
