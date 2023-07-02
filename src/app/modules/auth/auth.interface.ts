0649ca4c790bd8189e5131c4ca7aaeea9c534013
Kasib Mohammad Chowdhury <kasib.md.chy@gmail.com>
2023-07-02 09:54:18 +0600

    implemented jwt accessToken, refreshToken, hashed password and changed password

--- src/app/modules/auth/auth.interface.ts
+++ src/app/modules/auth/auth.interface.ts
@@ -15,4 +15,9 @@ export type IRefreshTokenResponse = {
   accessToken: string;
 };
 
+export type IChangePassword = {
+  oldPassword: string;
+  newPassword: string;
+};
+
 export type LoginUserModel = Model<ILoginUser, Record<string, unknown>>;
